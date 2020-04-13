7cc9d506e33c070ef594a80c3525c9723e15d8e0
kwambokaB <paula.paulatek@gmail.com>
2020-04-13 19:29:18 +0300

    challenge4

--- src/estimator.js
+++ src/estimator.js
@@ -89,18 +89,4 @@ const covid19ImpactEstimator = (data) => ({
   }
 });
 
-const dataInput = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-
-};
-covid19ImpactEstimator(dataInput);
+export default covid19ImpactEstimator;
