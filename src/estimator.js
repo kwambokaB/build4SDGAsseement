0d1da18bfaed7e2357fcdaa59851df19117bd5ea
kwambokaB <paula.paulatek@gmail.com>
2020-04-13 09:00:58 +0300

    challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,25 @@
-const covid19ImpactEstimator = (data) => data;
-
+const factor = (data) => {
+  let getFactor;
+  if (data.periodType.trim().toLowerCase() === 'days') {
+    getFactor = Math.trunc((data.timeToElapse * 1) / 3);
+  } else if (data.periodType.trim().toLowerCase() === 'weeks') {
+    getFactor = Math.trunc((data.timeToElapse * 7) / 3);
+  } else if (data.periodType.trim().toLowerCase() === 'months') {
+    getFactor = Math.trunc((data.timeToElapse * 30) / 3);
+  } else {
+    getFactor = 1;
+  }
+  return getFactor;
+};
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** factor(data))
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** factor(data))
+  }
+});
 export default covid19ImpactEstimator;
